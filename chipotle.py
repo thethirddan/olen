ec19f094a3654e3091a60460abf7f43120989373
Dan <dan@supio.com>
2025-10-14 13:39:26 -0700

    cleaner

--- chipotle.py
+++ chipotle.py
@@ -1,29 +1,61 @@
 from enum import Enum
 
 class Protein(Enum):
-    CHICKEN = 'chicken'
-    CARNITAS = 'carnitas'
-    STEAK = 'steak'
-    BARBACOA = 'barbacoa'
-    VEGGIES = 'veggies'
+    CHICKEN = ('chicken', 2.5)
+    CARNITAS = ('carnitas', 3.0)
+    STEAK = ('steak', 3.5)
+    BARBACOA = ('barbacoa', 3.5)
+    VEGGIES = ('veggies', 2.5)
+
+    @property
+    def name(self):
+        return self.value[0]
+
+    @property
+    def price(self):
+        return self.value[1]
 
 class Rice(Enum):
-    WHITE = 'white'
-    BROWN = 'brown'
+    WHITE = ('white', 2.5)
+    BROWN = ('brown', 3.5)
+
+    @property
+    def name(self):
+        return self.value[0]
+
+    @property
+    def price(self):
+        return self.value[1]
 
 class Beans(Enum):
-    PINTO = 'pinto'
-    BLACK = 'black'
+    PINTO = ('pinto', 2.5)
+    BLACK = ('black', 2.5)
+
+    @property
+    def name(self):
+        return self.value[0]
+
+    @property
+    def price(self):
+        return self.value[1]
 
 class Toppings(Enum):
-    PINTO = 'queso blanco'
-    CHEESE = 'cheese'
-    FAJITA_VEGGIES = 'fajita veggies'
-    SOUR_CREAM = 'sour cream'
-    GUACAMOLE = 'guacamole'
-    TOMATO_SALSA = 'tomato salsa'
-    CHILI_SALSA = 'chili corn salsa'
-    TOMATILLO_SALSA = 'tomatillo green chili salsa'
+    QUESO_BLANCO = ('queso blanco', 2.75)
+    CHEESE = ('cheese', 2.0)
+    FAJITA_VEGGIES = ('fajita veggies', 2.5)
+    SOUR_CREAM = ('sour cream', 2.5)
+    GUACAMOLE = ('guacamole', 2.75)
+    TOMATO_SALSA = ('tomato salsa', 2.5)
+    CHILI_SALSA = ('chili corn salsa', 1.75)
+    TOMATILLO_SALSA = ('tomatillo green chili salsa', 0.0)
+
+    @property
+    def name(self):
+        return self.value[0]
+
+    @property
+    def price(self):
+        return self.value[1]
 
 class Locations(Enum):
    AMHERST = 'amherst'
@@ -39,36 +71,39 @@ class Locations(Enum):
    DEERFIELD = 'deerfield'
    SPRINGFIELD = 'springfield'
 
-class Price(Enum):
-    PROTEIN_CHICKEN = 2.5
-    PROTEIN_CARNITAS = 3
-    PROTEIN_STEAK = 3.5
-    PROTEIN_BARBACOA = 3.5
-    PROTEIN_VEGGIES = 2.5
-    RICE_WHITE = 2.5
-    RICE_BROWN = 3.5
-
 order1 = ('manan', 'holyoke', 'FLAT3', 'chicken', 'white', 'pinto', False, 'queso blanco', 'cheese', 'fajita veggies', 'sour cream')
 order2 = ('allison', 'greenfield', 'MAGIC', 'carnitas', 'brown', 'black', True, 'cheese', 'fajita veggies', 'sour cream', 'guacamole', 'tomato salsa')
 
 
 #get_protein_price
 def get_protein(order):
-    protein = order(3)
-    protein_key = 'PROTEIN_' + protein.upper()
-    if (Price[protein_key]):
-        return Price[protein_key]
-    else:
+    user_protein = order[3].upper()
+    try:
+        lookup_protein = Protein[user_protein]
+        return lookup_protein.price
+    except KeyError:
         # probably print a warning here
+        print(f"Warning: Unknown protein '{user_protein}'")
         return 0
 
 def get_rice(order):
-    rice = order(4)
-    rice_key = 'RICE_' + rice.upper()
-    if (Price[rice_key]):
-        return Price[rice_key]
-    else:
+    user_rice = order[4].upper()
+    try:
+        lookup_rice = Rice[user_rice]
+        return lookup_rice.price
+    except KeyError:
         # probably print a warning here
+        print(f"Warning: Unknown rice '{user_rice}'")
+        return 0
+
+def get_beans(order):
+    user_beans = order[5].upper()
+    try:
+        lookup_beans = Beans[user_beans]
+        return lookup_beans.price
+    except KeyError:
+        # probably print a warning here
+        print(f"Warning: Unknown beans '{user_beans}'")
         return 0
     
 print(get_protein(order1))
